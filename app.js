fd7fcc0b06c04c3866bb22719cf4d81c6f64706c
JuniorL08 <junior-18@hotmail.es>
2025-02-27 00:05:13 -0400

    first comment eliminated

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Variables globales
 let cantidadDeAmigos = 5;
 let amigos = [];
